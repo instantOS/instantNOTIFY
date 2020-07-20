8987f0e14dc18ab8c5263f4bbbda6257e696cc87
paperbenni <paperbenni@gmail.com>
2020-07-20 11:37:43 +0200

    ctl feature to delete all from application

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+installscript() {
+    [ -e "$1" ] || return 1
+    cat "$1" | sudo tee /usr/bin/"${2:-$1}"
+    sudo chmod 755 /usr/bin/"${2:-$1}"
+}
+
+installscript instantnotifyctl
+installscript instantnotify
+installscript instantnotifytrigger
+installscript instantnotifytrigger dunsttrigger
